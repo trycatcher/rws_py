8e045b079decdff4e86f37fa7eb46765714e61b4
unknown <AKHANRA@LIN41000874.corp.capgemini.com>
2014-07-22 16:41:52 +0530

    Added handling for non-200 responses

--- chapter1/book_search.py
+++ chapter1/book_search.py
@@ -21,49 +21,53 @@ The response format from the API call:-
         {...}]
 }
 '''
-books_json = requests.get('http://openlibrary.org/search.json?q=calcutta+chromosome')
+books_request = requests.get('http://openlibrary.org/search.json?q=calcutta+chromosome')
 
-# Representing the JSON response as a Python data structure so that it is easier to manipulate using language primitives.
-# 'docs' is the JSON elemnet we are interested in
-books_data = json.loads(books_json.text)
-docs = books_data['docs']
+if books_request.status_code == '200':
+    books_json = books_request
 
-'''
-Each document in "docs" is of the format
-Each document specified listed in "docs" will be of the following format: 
+    # Representing the JSON response as a Python data structure so that it is easier to manipulate using language primitives.
+    # 'docs' is the JSON elemnet we are interested in
+    books_data = json.loads(books_json.text)
+    docs = books_data['docs']
 
-{
-    cover_i: 258027,
-    has_fulltext: true,
-    edition_count: 120,
-    title: "The Lord of the Rings",
-    author_name: [
-        "J. R. R. Tolkien"
-    ],
-    first_publish_year: 1954,
-    key: "OL27448W",
-    ia: [
-        "returnofking00tolk_1",
-        "lordofrings00tolk_1",
-        "lordofrings00tolk_0",
-        "lordofrings00tolk_3",
-        "lordofrings00tolk_2",
-        "lordofrings00tolk",
-        "twotowersbeingse1970tolk",
-        "lordofring00tolk",
-        "lordofrings56tolk",
-        "lordofringstolk00tolk",
-        "fellowshipofring00tolk_0"
-    ],
-    author_key: [
-        "OL26320A"
-    ],
-    public_scan_b: true
-}
-'''
-for doc in docs:
-	print('"%s" by %s' %(doc['title'], ", ".join(doc['author_name'])))
+    '''
+    Each document in "docs" is of the format
+    Each document specified listed in "docs" will be of the following format: 
 
+    {
+        cover_i: 258027,
+        has_fulltext: true,
+        edition_count: 120,
+        title: "The Lord of the Rings",
+        author_name: [
+            "J. R. R. Tolkien"
+        ],
+        first_publish_year: 1954,
+        key: "OL27448W",
+        ia: [
+            "returnofking00tolk_1",
+            "lordofrings00tolk_1",
+            "lordofrings00tolk_0",
+            "lordofrings00tolk_3",
+            "lordofrings00tolk_2",
+            "lordofrings00tolk",
+            "twotowersbeingse1970tolk",
+            "lordofring00tolk",
+            "lordofrings56tolk",
+            "lordofringstolk00tolk",
+            "fellowshipofring00tolk_0"
+        ],
+        author_key: [
+            "OL26320A"
+        ],
+        public_scan_b: true
+    }
+    '''
+    for doc in docs:
+    	print('"%s" by %s' %(doc['title'], ", ".join(doc['author_name'])))
+else:
+    print('Searching books is not working now. Please try later.')
 
 
 '''
